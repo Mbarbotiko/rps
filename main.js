03fbd268ca18ae3e18671f465968d397b8b59962
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-10-05 07:10:33 -0500

    adding initial js and HTML to set up the game

--- main.js
+++ main.js
@@ -1,4 +1,53 @@
-$(document).ready(function () {
 
-});
+var computerChoices = ['r', 'p', 's'];
+var wins = 0;
+var losses = 0;
+var ties = 0;
+var gamesLeft = 10;
+var youTheChamp = null;
 
+
+//on this function the game starts, user will push a key down and we will get some action!
+document.onkeydown = function (event) {
+    var userGuess = String.fromCharCode(event.which).toLowerCase();
+    //captures user input and changes it to lowercase if they happened to enter it as uppercase
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    //computers guess willbe random, using math random (floor to get an integer without a decimal) random number the length of the computer choices array(3)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
